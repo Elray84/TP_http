767380dd9de588c094eb8ff895c48738b892f3a2
Elian <elianraynaud@gmail.com>
2019-10-11 15:04:11 +0200

    Implement class Salutation + Server

---
+++ src/main/java/Salutation.java
@@ -0,0 +1,32 @@
+import org.apache.cxf.jaxrs.JAXRSServerFactoryBean;
+import org.apache.cxf.jaxrs.lifecycle.SingletonResourceProvider;
+
+import javax.ws.rs.*;
+import java.util.Scanner;
+
+@Path("/bonjour")
+public class Salutation {
+
+    @GET
+    @Produces("text/plain")
+    @Consumes("text/plain")
+    public String getSalutation(@QueryParam("nom") String nom){
+        return "Salutations mon cher " + nom;
+    }
+
+    public static void main(String[] args) {
+        JAXRSServerFactoryBean sf = new JAXRSServerFactoryBean();
+        sf.setResourceClasses(Salutation.class);
+        sf.setResourceProvider(
+                Salutation.class,
+                new SingletonResourceProvider(new Salutation())
+        );
+        sf.setAddress("http://localhost:9000/");
+        sf.create();
+
+        System.out.println("Saisir car+return pour stopper le serveur");
+        new Scanner(System.in).next();
+
+        System.out.println("Fin");
+    }
+}
